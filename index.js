a2d1dbb3d100981b62072983efff12207da286a5
lbbiplob <biplab.jsnu@gmail.com>
2022-11-24 20:39:20 +0600

    jwt token added

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ const run = async () => {
   try {
     const usersCollection = client.db("mobileDB").collection("users");
 
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: " " });
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
